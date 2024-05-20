887f1e8e6b64a81fa07c12bc9c5749762bef0303
Chae Kim <chaekim0820@gmail.com>
2024-05-19 21:43:24 -0700

    more redirections

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>basic change to get rid of the default page</p>
   );
 }
 
