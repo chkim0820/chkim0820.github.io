3c1be64edde51d1ae9a8923117d329b8452d6208
Chae Kim <chaekim0820@gmail.com>
2024-12-27 00:48:28 -0800

    Background images added for grids

--- personal-site/src/components/projects.js
+++ personal-site/src/components/projects.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import linkedinLogo from '../assets/linkedin-logo.png';
 import githubLogo from '../assets/github-logo.png';
+import artifactImage from '../assets/project/artifact.png'
+import interpreterImage from '../assets/project/interpreter.png'
+import maritimeImage from '../assets/project/maritime.png'
+import websiteImage from '../assets/project/website.png'
+
 
 function Projects() {
   return (
@@ -31,7 +36,34 @@ function Projects() {
                 </nav>
             </header>
             <main>
-            {/* Insert stuff here */}
+            <h1>Projects</h1>
+            <div class="grid-container">
+                <div class="grid-item project1">
+                    <h3 class="project-name">chkim0820.github.io</h3>
+                    <p class="description">This website</p>
+                    <a href="https://github.com/chkim0820/chkim0820.github.io/tree/main/personal-site" class="project-link" target="_blank">Source</a>
+                </div>
+                <div class="grid-item project2">
+                    <h3 class="project-name">Language Interpreter</h3>
+                    <p class="description">Racket interpreter for a Java/C-like language</p>
+                    <a href="https://github.com/chkim0820/Programming_Language_Concepts/tree/main/Interpreters" class="project-link" target="_blank">Source</a>
+                </div>
+                <div class="grid-item project3">
+                    <h3 class="project-name">Maritime AIS Predictor</h3>
+                    <p class="description">ML clustering algorithm analysis to predict movement</p>
+                    <a href="https://github.com/chkim0820/Intro_to_ML/tree/main/Case_Study_2" class="project-link" target="_blank">Source</a>
+                </div>
+                <div class="grid-item project4">
+                    <h3 class="project-name">Artifacts of the Past</h3>
+                    <p class="description">2D platform game to navigate obstacles toward victory</p>
+                    <a href="https://ecse-csds290.itch.io/artifacts-of-the-past-2023-team-7" class="project-link" target="_blank">Demo</a>
+                </div>
+                {/* <div class="grid-item">
+                    <h3 class="project-name">AI Agent Hostility Simulator</h3>
+                    <p class="description">Multi-agent simulation to violence depending on resource availability</p>
+                    <a href="https://example.com/project1" class="project-link" target="_blank">Source</a>
+                </div> */}
+            </div>
             </main>
             <footer>
                 <p>© 2024 Chaehyeon Kim</p>
